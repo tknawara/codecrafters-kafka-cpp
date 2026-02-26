c8c038e5d0ba008cb84e2dcacf3f2b863024fbd1
tknawara <tarek.nawara@gmail.com>
2026-02-26 23:21:14 +0000

    codecrafters submit [skip ci]

--- src/serializable.hpp
+++ src/serializable.hpp
@@ -6,7 +6,7 @@
 
 namespace kafka {
 template <typename T>
-concept Serializable = requires(const T &item, std::vector<uint8_t> &buffer) {
+concept Serializable = requires(std::vector<uint8_t> &buffer, const T &item) {
   // Enforce that a free function exists and is callable
   { serialize(buffer, item) } -> std::same_as<void>;
 };
